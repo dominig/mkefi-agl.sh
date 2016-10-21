03a9f8e0d230c290ac7a2e884e1a4ead1fe8d09f
Dominig ar Foll <dominig.arfoll@fridu.net>
2016-10-21 20:31:41 +0200

    adding systemd-boot / gummyboot support

--- mkefi-agl.sh
+++ mkefi-agl.sh
@@ -319,8 +319,8 @@ else
 	mkfs.vfat $BOOTFS -n "EFI" >$OUT 2>&1 || die "Failed to format $BOOTFS"
 fi
 
-debug "Formatting $ROOTFS as ext3"
-mkfs.ext3 -F $ROOTFS -L "ROOT" >$OUT 2>&1 || die "Failed to format $ROOTFS"
+debug "Formatting $ROOTFS as ext4"
+mkfs.ext4 -F $ROOTFS -L "ROOT" >$OUT 2>&1 || die "Failed to format $ROOTFS"
 
 
 #
@@ -341,7 +341,7 @@ fi
 echo "bootx64.efi" > $BOOTFS_MNT/startup.nsh || error "Failed to create startup.nsh"
 # Copy the efi loader and configs (booti*.efi and grub.cfg if it exists)
 cp -r $HDDIMG_MNT/EFI $BOOTFS_MNT >$OUT 2>&1 || error "Failed to copy EFI dir"
-# Silently ignore a missing gummiboot loader dir (we might just be a GRUB image)
+# Silently ignore a missing systemd-boot or gummiboot loader dir (we might just be a GRUB image)
 cp -r $HDDIMG_MNT/loader $BOOTFS_MNT >$OUT 2>&1
 
 # Update the boot loaders configurations for an installed image
@@ -362,12 +362,38 @@ if [ -e "$GRUB_CFG" ]; then
 	sed -i "s/ LABEL=[^ ]*/ /" $GRUB_CFG
 
 	sed -i "s@ root=[^ ]*@ @" $GRUB_CFG
-	sed -i "s@vmlinuz @vmlinuz root=$ROOTFS_PARTUUID ro rootwait quiet @" $GRUB_CFG
+	sed -i "s@vmlinuz @vmlinuz root=$ROOTFS_PARTUUID ro quiet rootwait @" $GRUB_CFG
 fi
+
+# look for a systemd-boot loader.conf file and create a default boot entry
+SYSTEDBOOT_CFG="$BOOTFS_MNT/loader/loader.conf"
+SYSTEDBOOT_BOOT="$BOOTFS_MNT/loader/entries/boot.conf"
+SYSTEDBOOT_DEBUG="$BOOTFS_MNT/loader/entries/debug.conf"
+if [ -e "$SYSTEDBOOT_CFG" ]; then
+	info "Configuring SYSTEMD-BOOT"
+	# Delete any existing entries
+	rm -rf "$BOOTFS_MNT/loader/entries" >$OUT 2>&1
+	mkdir  "$BOOTFS_MNT/loader/entries" >$OUT 2>&1
+	# create the new loader.conf file
+	echo "# Created by mkefi-agl.sh script `date`" > $SYSTEDBOOT_CFG
+	echo "default boot" >> $SYSTEDBOOT_CFG
+	echo "timout 5" >> $SYSTEDBOOT_CFG
+	# create the boot entry
+	echo "title boot"  > $SYSTEDBOOT_BOOT
+	echo "linux /vmlinuz" >> $SYSTEDBOOT_BOOT
+	echo "initrd /initrd" >> $SYSTEDBOOT_BOOT
+	echo "options LABEL=boot root=$ROOTFS_PARTUUID ro quiet rootwait console=ttyS0,115200 console=tty0" >> $SYSTEDBOOT_BOOT
+	# create the debug entry
+	echo "title debug" > $SYSTEDBOOT_DEBUG
+	echo "linux /vmlinuz" >> $SYSTEDBOOT_DEBUG
+	echo "initrd /initrd" >> $SYSTEDBOOT_DEBUG
+	echo "options LABEL=debug root=$ROOTFS_PARTUUID ro debug rootwait console=ttyS0,115200 console=tty0" >> $SYSTEDBOOT_DEBUG
+	
+fi	
 
 
 # Ensure we have at least one EFI bootloader configured
-if [ ! -e $GRUB_CFG ] ; then
+if [ ! -e $GRUB_CFG ]  && [ ! -e $SYSTEDBOOT_CFG ] ; then
 	die "No EFI bootloader configuration found"
 fi
 
